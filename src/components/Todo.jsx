a558c3f6cb23e2114244aa10fae388a5c80c20ec
AnkitSingh60 <93372584+AnkitSingh60@users.noreply.github.com>
2023-01-06 16:05:25 +0530

    final push

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
-import closeIcon from "../images/close.svg"
-import checkIcon from "../images/check.svg"
-import {completeTodo, removeTodo} from "../redux/features/todo/todoSlice"
-import {useDispatch} from "react-redux"
+import React from "react";
+import closeIcon from "../images/close.svg";
+import checkIcon from "../images/check.svg";
+import { completeTodo, removeTodo } from "../redux/features/todo/todoSlice";
+import { useDispatch } from "react-redux";
 
-const Todo = ({content, completed, id}) => {
+const Todo = ({ content, completed, id }) => {
   const dispatch = useDispatch();
 
   const completeTodoHandler = () => {
-    dispatch(completeTodo(id))
-  }
+    dispatch(completeTodo(id));
+  };
   const removeTodoHandler = () => {
-    dispatch(removeTodo(id))
-  }
+    dispatch(removeTodo(id));
+  };
 
   return (
-    <div className='todo_container' onClick={completeTodoHandler}>
+    <div className="todo_container" onClick={completeTodoHandler}>
       <div className={`circle ${completed ? "active" : ""}`}>
         <img src={checkIcon} className="svgImg" alt="check" />
       </div>
       <li className={`todo ${completed ? "active" : ""}`}>{content}</li>
-      <img className='delete-icon svgImg' src={closeIcon} alt="close" onClick={removeTodoHandler} />
+      <span class="material-symbols-outlined editIcon">edit_square</span>
+      <img
+        className="delete-icon svgImg"
+        src={closeIcon}
+        alt="close"
+        onClick={removeTodoHandler}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
